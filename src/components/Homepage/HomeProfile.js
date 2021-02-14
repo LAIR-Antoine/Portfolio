e45f54ed7068008266829a25ea1a848f366be47e
LAIR-Antoine <antoine.lair@utt.fr>
2021-02-14 18:53:58 +0100

    Profile and HomeProfile Mobile

--- src/components/Homepage/HomeProfile.js
+++ src/components/Homepage/HomeProfile.js
@@ -25,15 +25,17 @@ class HomeProfile extends Component {
 
         <h2>Bienvenue sur mon portfolio !</h2>
 
-        <svg className="suite-button" width="125" height="125" viewBox="0 0 180 180">
-          <g id="Rectangle_1" transform="translate(86.005) rotate(45)">
-            <rect x="5.93" y="-0.12" transform="matrix(2.535182e-06 1 -1 2.535182e-06 127.4374 -6.0445)" className="st3" width="121.63" height="121.63"/>
-            <rect x="8.43" y="2.38" transform="matrix(2.535182e-06 1 -1 2.535182e-06 127.4374 -6.0445)" className="st4" width="116.63" height="116.63"/>
-          </g>
-          <line id="Ligne_5" className="st5" x1="45.58" y1="90.26" x2="136.73" y2="90.26"/>
-          <line id="Ligne_6" className="st5" x1="99.95" y1="129.31" x2="137.55" y2="91.72"/>
-          <line id="Ligne_7" className="st5" x1="99.95" y1="53.23" x2="137.55" y2="90.83"/>
-        </svg>
+        <a href="/profile">
+          <svg className="suite-button" viewBox="0 0 180 180">
+            <g id="Rectangle_1" transform="translate(86.005) rotate(45)">
+              <rect x="5.93" y="-0.12" transform="matrix(2.535182e-06 1 -1 2.535182e-06 127.4374 -6.0445)" className="st3" width="121.63" height="121.63"/>
+              <rect x="8.43" y="2.38" transform="matrix(2.535182e-06 1 -1 2.535182e-06 127.4374 -6.0445)" className="st4" width="116.63" height="116.63"/>
+            </g>
+            <line id="Ligne_5" className="st5" x1="45.58" y1="90.26" x2="136.73" y2="90.26"/>
+            <line id="Ligne_6" className="st5" x1="99.95" y1="129.31" x2="137.55" y2="91.72"/>
+            <line id="Ligne_7" className="st5" x1="99.95" y1="53.23" x2="137.55" y2="90.83"/>
+          </svg>
+        </a>
 
         <div className="SocialLinks">
           <a href="https://www.facebook.com/antoine.lair.3/" target="_blank" rel="noopener noreferrer" alt="Facebook"><i className="fab fa-facebook"></i></a>
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import './Profile.css';
+
+class Profile extends Component {
+
+  render () {
+    return (
+      <div className="Profile">
+
+        <svg className="burger-menu" width="125" height="125" viewBox="0 0 180 180">
+          <g id="Rectangle_1" transform="translate(86.005) rotate(45)">
+            <rect x="5.93" y="-0.12" transform="matrix(2.535182e-06 1 -1 2.535182e-06 127.4374 -6.0445)" className="st0" width="121.63" height="121.63"/>
+            <rect x="8.43" y="2.38" transform="matrix(2.535182e-06 1 -1 2.535182e-06 127.4374 -6.0445)" className="st1" width="116.63" height="116.63"/>
+          </g>
+          <line id="Ligne_1" className="st2" x1="44.56" y1="90.01" x2="135.79" y2="90.01"/>
+          <line id="Ligne_2" className="st2" x1="64.33" y1="67.2" x2="117.54" y2="67.2"/>
+          <line id="Ligne_3" className="st2" x1="64.33" y1="112.81" x2="117.54" y2="112.81"/>
+        </svg>
+
+        <h1>MON <span>PROFIL</span></h1>
+
+        <div className="LineOne">
+          {null}
+        </div>
+        <div className="ProfileContent">
+            <p className="justify">Bonjour à tous !<br /> Je suis Antoine LAIR, un <span className="linkcolor">étudiant</span> de 20 ans.</p>
+            <p className="justify">Depuis ma terminale, je fais du <span className="linkcolor">développement web</span>. En 2020, j'ai commencé à me diversifier en découvrant le <span className="linkcolor">graphisme</span> et le <span className="linkcolor">montage vidéo</span>.</p>
+            <p className="justify">J'aime bien prendre des <span className="linkcolor">photos</span> quand j'ai du temps libre.</p>
+            <p className="justify">Je fais du <span className="linkcolor">triathlon</span> depuis 8 ans et je participe à des <span className="linkcolor">postes de secours</span> depuis 1 an.</p>
+            <p className="See-more">Cliquez ici pour <span className="bold">en savoir plus</span> ou passer à la suite !</p>
+        </div>
+
+
+        <a href="/">
+          <svg className="suite-button-bottom" viewBox="0 0 180 180">
+            <g id="Rectangle_1" transform="translate(86.005) rotate(45)">
+              <rect x="5.93" y="-0.12" transform="matrix(2.535182e-06 1 -1 2.535182e-06 127.4374 -6.0445)" className="st3" width="121.63" height="121.63"/>
+              <rect x="8.43" y="2.38" transform="matrix(2.535182e-06 1 -1 2.535182e-06 127.4374 -6.0445)" className="st4" width="116.63" height="116.63"/>
+            </g>
+            <line id="Ligne_5" className="st5" x1="45.58" y1="90.26" x2="136.73" y2="90.26" transform="translate(180) rotate(90)"/>
+            <line id="Ligne_6" className="st5" x1="99.95" y1="129.31" x2="137.55" y2="91.72" transform="translate(180) rotate(90)"/>
+            <line id="Ligne_7" className="st5" x1="99.95" y1="53.23" x2="137.55" y2="90.83" transform="translate(180) rotate(90)"/>
+          </svg>
+        </a>
+        
+      </div>
+
+    );
+  }
+}
+
+export default Profile;
