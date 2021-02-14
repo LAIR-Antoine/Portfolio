0dc1455e854ca21d57580d421ade0ffe70fbfeab
LAIR-Antoine <antoine.lair@utt.fr>
2021-02-14 16:49:26 +0100

    Home for computer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import React, { Component } from 'react';
 import './App.css';
+import HomeProfile from './components/Homepage/HomeProfile'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+
+  render () {
+    return (
+      <div className="App">
+        <HomeProfile />
+      </div>
+
+    );
+  }
 }
 
 export default App;
