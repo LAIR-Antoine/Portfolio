e2663221f85b5346bdedb2282ed42d9b3b64140a
Antoine Lair <62932641+LAIR-Antoine@users.noreply.github.com>
2021-09-07 15:39:26 +0200

    Update MMI

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -16,7 +16,7 @@ class Profile extends Component {
           {null}
         </div>
         <div className="ProfileContent">
-            <p className="justify">Bonjour à tous !<br /> Je suis Antoine LAIR, un <span className="linkcolor">étudiant</span> de 20 ans.</p>
+            <p className="justify">Bonjour à tous !<br /> Je suis Antoine LAIR, 20 ans et <span className="linkcolor">étudiant</span> en <span className="linkcolor">BUT MMI</span> à Troyes.</p>
             <p className="justify">Depuis ma terminale, je fais du <span className="linkcolor">développement web</span>. En 2020, j'ai commencé à me diversifier en découvrant le <span className="linkcolor">graphisme</span>, le <span className="linkcolor">montage vidéo</span> et le <span className="linkcolor">réseau</span>.</p>
             <p className="justify">J'aime bien prendre des <span className="linkcolor">photos</span> quand j'ai du temps libre.</p>
             <p className="justify">Je fais du <span className="linkcolor">triathlon</span> depuis 8 ans et je participe à des <span className="linkcolor">postes de secours</span> depuis 1 an.</p>
